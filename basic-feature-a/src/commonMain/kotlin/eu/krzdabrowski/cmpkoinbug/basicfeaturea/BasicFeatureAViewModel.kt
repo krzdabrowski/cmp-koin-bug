d82a49e76481963e4ccbb2c9974031aa3961b25b
Krzysztof Dabrowski <krz.dabrowski95@gmail.com>
2025-06-08 19:21:38 +0200

    Add example ViewModels to :basic-feature-a, :basic-feature-b and :core modules

---
+++ basic-feature-a/src/commonMain/kotlin/eu/krzdabrowski/cmpkoinbug/basicfeaturea/BasicFeatureAViewModel.kt
@@ -0,0 +1,7 @@
+package eu.krzdabrowski.cmpkoinbug.basicfeaturea
+
+import eu.krzdabrowski.cmpkoinbug.core.BaseViewModel
+import org.koin.android.annotation.KoinViewModel
+
+@KoinViewModel
+internal class BasicFeatureAViewModel : BaseViewModel()
---
+++ core/src/commonMain/kotlin/eu/krzdabrowski/cmpkoinbug/core/BaseViewModel.kt
@@ -0,0 +1,5 @@
+package eu.krzdabrowski.cmpkoinbug.core
+
+import androidx.lifecycle.ViewModel
+
+abstract class BaseViewModel : ViewModel()
