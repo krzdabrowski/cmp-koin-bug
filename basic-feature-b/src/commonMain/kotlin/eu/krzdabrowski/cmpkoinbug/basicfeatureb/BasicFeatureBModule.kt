20f7eef0c3a1a60e850ce5ef10f303461e0f2aab
Krzysztof Dabrowski <krz.dabrowski95@gmail.com>
2025-06-08 21:00:06 +0200

    Add example Koin modules to :basic-feature-a and :basic-feature-b

---
+++ basic-feature-b/src/commonMain/kotlin/eu/krzdabrowski/cmpkoinbug/basicfeatureb/BasicFeatureBModule.kt
@@ -0,0 +1,8 @@
+package eu.krzdabrowski.cmpkoinbug.basicfeatureb
+
+import org.koin.core.annotation.ComponentScan
+import org.koin.core.annotation.Module
+
+@Module
+@ComponentScan("eu.krzdabrowski.cmpkoinbug.basicfeatureb")
+object BasicFeatureBModule
